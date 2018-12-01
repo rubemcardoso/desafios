1d85823826a3fecf3b1d90a775f56192beb556d2
Guihgo <guihgo100milha@gmail.com>
2018-12-01 11:41:11 -0200

    melhor visualizao no console

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var LoteriaClass = require('./Loteria');
 var JogoClass = require('./Jogo');
 
-var minhaLoteria = new LoteriaClass([45,13,1,18,33,35], 100000);
+var minhaLoteria = new LoteriaClass([45,13,1,18,33,69], 100000);
 
 // minhaLoteria.showSorteio();
 minhaLoteria.addJogo(new JogoClass([1,15,17,33,35,18]));
@@ -9,7 +9,7 @@ minhaLoteria.addJogo(new JogoClass([3,5,59,33,17,17]));
 minhaLoteria.addJogo(new JogoClass([45,13,1,18,33,35]));
 
 minhaLoteria.showJogos();
-console.log(minhaLoteria.calculaGanhador(4));
-console.log(minhaLoteria.maisJogado());
-console.log(minhaLoteria.naoJogado());
+console.log("Qnt de jogares que acertaram exatamente 4: ", minhaLoteria.calculaGanhador(4));
+console.log("Numeros mais jogados: ", minhaLoteria.maisJogado());
+console.log("Numeros nao jogados: ", minhaLoteria.naoJogado());
 minhaLoteria.resultado();
